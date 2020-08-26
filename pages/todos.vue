f8fe992b2bf6cdad40df74d3bd2292482c0e6f2a
nishina555 <n.toshi.1.014@gmail.com>
2020-08-26 14:50:03 +0900

    dev

--- pages/todos.vue
+++ pages/todos.vue
@@ -14,6 +14,11 @@
 import TodoList from '@/components/TodoList'
 
 export default {
+  data() {
+    return {
+      title: "",
+    }
+  },
   async asyncData({ app }) {
     const { data } = await app.$axios.get(`http://localhost:4001/todos`)
     return { todos: data }
@@ -25,9 +30,18 @@ export default {
         is_completed: false,
       };
       await this.$axios.post(`http://localhost:4001/todos`, todo);
+      const { data } = await this.$axios.get(`http://localhost:4001/todos`);
+      // const data = await this.getTodos();
+
+      this.todos = data;
+      this.title="";
       // TODO 画面のリロード
       // TODO postしたらフォームからcreateを実行できないように制御する
-    }
+    },
+    // async getTodos() {
+    //   const { data } = await this.$axios.get(`http://localhost:4001/todos`);
+    //   return data;
+    // }
   }
 
 }
