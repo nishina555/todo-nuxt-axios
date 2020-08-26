265ff1cdff05c9131dd276142fdaebe5f58362d3
nishina555 <n.toshi.1.014@gmail.com>
2020-08-26 09:36:53 +0900

    get and show todos from api

---
+++ components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="todo in todos" :key="todo.id">
+      <div class="container">
+        <p>{{ todo.title }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    todos: {
+      type: Array
+    }
+  },
+}
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+}
+</style>
--- pages/todos.vue
+++ pages/todos.vue
@@ -3,18 +3,25 @@
     <h2>
       todolist
     </h2>
+    <todo-list
+      :todos="todos"
+    />
   </div>
 </template>
 
 <script>
+import TodoList from '@/components/TodoList'
+
 export default {
+  async asyncData({ app }) {
+    const { data } = await app.$axios.get(`http://localhost:4001/todos`)
+    return { todos: data }
+  }
 }
 </script>
 
 <style scoped>
 .container {
-  /* margin: 0 auto; */
-  /* width: 50%; */
   text-align: center;
 }
 </style>
