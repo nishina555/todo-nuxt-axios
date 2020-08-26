5ac97e238200e42d8765b19b2f0c0c5c7e737ae8
nishina555 <n.toshi.1.014@gmail.com>
2020-08-26 09:33:12 +0900

    create todos page

---
+++ pages/todos.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h2>
+      todolist
+    </h2>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+.container {
+  /* margin: 0 auto; */
+  /* width: 50%; */
+  text-align: center;
+}
+</style>
