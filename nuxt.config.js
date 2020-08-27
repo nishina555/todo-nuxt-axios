258ee838034fc019238d675ff483b48583576c21
nishina555 <n.toshi.1.014@gmail.com>
2020-08-27 09:39:05 +0900

    add vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,4 @@
+import colors from 'vuetify/es5/util/colors'
 
 export default {
   /*
@@ -45,6 +46,7 @@ export default {
   ** Nuxt.js dev-modules
   */
   buildModules: [
+    '@nuxtjs/vuetify',
   ],
   /*
   ** Nuxt.js modules
@@ -58,6 +60,27 @@ export default {
   ** See https://axios.nuxtjs.org/options
   */
   axios: {},
+    /*
+  ** vuetify module configuration
+  ** https://github.com/nuxt-community/vuetify-module
+  */
+ vuetify: {
+  customVariables: ['~/assets/variables.scss'],
+  theme: {
+    dark: true,
+    themes: {
+      dark: {
+        primary: colors.blue.darken2,
+        accent: colors.grey.darken3,
+        secondary: colors.amber.darken3,
+        info: colors.teal.lighten1,
+        warning: colors.amber.base,
+        error: colors.deepOrange.accent4,
+        success: colors.green.accent3
+      }
+    }
+  }
+},
   /*
   ** Build configuration
   ** See https://nuxtjs.org/api/configuration-build/
